83c943d262ce6f16da99f40819ac260f987e2bbf
Mari Calmer <superlassig@gmail.com>
2023-12-07 18:01:36 +0100

    feat: create build function for breadcrumb

--- components/Breadcrumb.jsx
+++ components/Breadcrumb.jsx
@@ -19,6 +19,13 @@ const buildInactiveMenu = (menuItem) => {
   )
 };
 
+const buildBreadcrumb = (activeMenuItem) => {
+  const menus = ["Digital", "Physical", "About", "Home"];
+  for (let menu of menus) {
+    if
+  }
+};
+
 function Breadcrumb(props) {
   return (
     <ul className="navlist__container flex helvetica place-content-between w-[80%] pt-8 pl-14 leading-none">

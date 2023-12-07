4eaf480b30ea96640664589e6fb378da9b1e5c2a
Mari Calmer <superlassig@gmail.com>
2023-12-07 17:56:44 +0100

    feat: create menu element factory functions of breadcrumb

--- components/Breadcrumb.jsx
+++ components/Breadcrumb.jsx
@@ -3,6 +3,22 @@
 import React from "react";
 import Link from 'next/link'
 
+const buildActiveMenu = (menuItem) => {
+  return (
+    <li className={"opacity-0 navlist__item"}>
+      {menuItem.charAt(0).toUpperCase() + menuItem.slice(1)}
+    </li>
+  )
+};
+
+const buildInactiveMenu = (menuItem) => {
+  return (
+    <li className="navlist__item hover:text-[#C9D1D2]">
+      <Link href={`/${menuItem.toLowerCase()}`}>{menuItem.charAt(0).toUpperCase() + menuItem.slice(1)}</Link>
+    </li>
+  )
+};
+
 function Breadcrumb(props) {
   return (
     <ul className="navlist__container flex helvetica place-content-between w-[80%] pt-8 pl-14 leading-none">

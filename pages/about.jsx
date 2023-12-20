d1d347ba4163e590b530c2c1feed215eebc33511
Mari Calmer <superlassig@gmail.com>
2023-12-20 16:26:41 +0100

    feat: add external links to about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -13,10 +13,14 @@ export default function About() {
   return (
     <div lang="en" className={`${quella.variable} ${helvetica.variable}`}>
       <Breadcrumb textColor="text-[#4A2E69]" activeItem="About"/>
-      <h1>Say Hello</h1>
-      <Link href="mailto:info@mailgo.dev" className="">Email</Link>
-      <Link href="www.instagram.com" className="">Instagram</Link>
-      <Link href="www.linkedin.com" className="">LinkedIn</Link>
+      <div className="pl-14">
+        <h1 className="text-2xl">Say Hello</h1>
+        <ul>
+          <li><Link href="mailto:info@mailgo.dev" className="text-xl">Email</Link></li>
+          <li><a href="https://www.instagram.com/studio.stuckn" target="_blank" className="text-xl">Instagram</a></li>
+          <li><a href="https://de.linkedin.com/in/ronja-stucken" target="_blank" className="text-xl">LinkedIn</a></li>
+        </ul>
+      </div>
       <StaticBrand opacity={"opacity-1"} />
     </div>
   )

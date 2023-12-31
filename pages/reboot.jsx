1cf2f2ada7f9430b2c69ee575afb3ed24ced6bf5
Mari Calmer <superlassig@gmail.com>
2023-12-31 14:48:24 +0100

    feat: update pics for reboot

--- pages/reboot.jsx
+++ pages/reboot.jsx
@@ -1,5 +1,61 @@
-export default function Reboot() {
+'use client';
+import reboot1 from '../public/img/physical/reboot/female_model_hiding_head_in_front_of_arms.webp'
+import reboot2 from '../public/img/physical/reboot/male_model_looks_into_tilted_camera.webp'
+import reboot3 from '../public/img/physical/reboot/full_body_view_of_female_model_from_the_front.webp'
+import reboot4 from '../public/img/physical/reboot/female_and_male_face_close_up.webp'
+import reboot5 from '../public/img/physical/reboot/male_model_shown_from_the_back_infront_of_sunreflection.webp'
+import reboot6 from '../public/img/physical/female_model_bust_shot.webp'
+import reboot7 from '../public/img/physical/reboot/male_model_in_motion.webp'
+import reboot8 from '../public/img/physical/reboot/male_model_full_body_half_profile.webp'
+import reboot9 from '../public/img/physical/reboot/male_model_bust_shot.webp'
+import reboot10 from '../public/img/physical/reboot/female_model_from_half_profile_full_body.webp'
+import reboot11 from '../public/img/physical/reboot/2_models_shown_full_body_looking_into_camera.webp'
+import reboot12 from '../public/img/physical/reboot/male_model_turning_photographed_from_the_back.webp'
+import reboot13 from '../public/img/physical/reboot/detail_view_of_trousrs_on_male_model.webp'
+import reboot14 from '../public/img/physical/reboot/full_body_view_of_female_model_from_the_back.webp'
+import reboot15 from '../public/img/physical/reboot/male_model_full_body_view.webp'
+import reboot16 from '../public/img/physical/reboot/female_model_looks_into_tilted_camera.webp'
+import reboot17 from '../public/img/physical/reboot/female_model_photographed_from_the_side_profile.webp'
+
+import Breadcrumb from "@/components/Breadcrumb";
+import Carousel from "@/components/Carousel";
+import StaticBrand from "@/components/StaticBrand";
+import PageContainer from "@/components/PageContainer";
+
+import "../public/stylesheets/projects.css";
+
+const images = [
+  { src: reboot1, alt: 'female model hiding head in front of arms' },
+  { src: reboot2, alt: 'male model looks into tilted camera' },
+  { src: reboot3, alt: 'full body view of female model from the front' },
+  { src: reboot4, alt: 'female and male face close up' },
+  { src: reboot5, alt: 'male model shown from the back in front of sun reflection' },
+  { src: reboot6, alt: 'female model bust shot' },
+  { src: reboot7, alt: 'male model in motion' },
+  { src: reboot8, alt: 'male model full body half profile' },
+  { src: reboot9, alt: 'male model bust shot' },
+  { src: reboot10, alt: 'female model from half profile full body' },
+  { src: reboot11, alt: '2 models shown full body looking into camera' },
+  { src: reboot12, alt: 'male model turning photographed from the back' },
+  { src: reboot13, alt: 'detail view of trousers on male model' },
+  { src: reboot14, alt: 'full body view of female model from the back' },
+  { src: reboot15, alt: 'male model full body view' },
+  { src: reboot16, alt: 'female model looks into tilted camera' },
+  { src: reboot17, alt: 'female model photographed from the side profile' }
+];
+
+const subtitle = "Just as a computer may crash due to overload or software glitches, humans may face personal crises or challenges that necessitate a restart or reevaluation of their lives. The act of rebooting in the human context implies a deliberate and conscious effort to initiate change, discard outdated patterns, and embrace a revitalized version of oneself.";
+const descriptionText = "Fashion & Styling Studio.Stuckn Photo Jaap Bräutigam Models Lea Rösch & Ringo Lukas Makeup Ekaterina Igonina";
+const description = { title: "Reboot", subtitle: subtitle, text: descriptionText };
+
+function Reboot() {
   return (
-    <h1>reboot</h1>
-  )
+    <PageContainer>
+      <Breadcrumb />
+      <Carousel images={images} description={description}/>
+      <StaticBrand opacity={"opacity-1"} />
+    </PageContainer>
+  );
 }
+
+export default Reboot;
--- pages/seefashion.jsx
+++ pages/seefashion.jsx
@@ -1,5 +1,49 @@
-export default function Seefashion() {
+'use client';
+import seeFashion1 from '../public/img/flux_carousel_1.webp'
+import seeFashion2 from '../public/img/physical/seefashion/model_bust_shot_looking_into_the_camera.webp'
+import seeFashion3 from '../public/img/physical/seefashion/diverse_model_indress.webp'
+import seeFashion4 from '../public/img/physical/seefashion/diverse_model_looking_to_the_side.webp'
+import seeFashion5 from '../public/img/physical/seefashion/3_model_looking_into_the_camera.webp'
+import seeFashion6 from '../public/img/physical/seefashion/runway_shot_of_model_walking.webp'
+import seeFashion7 from '../public/img/physical/seefashion/diverse_model_in_dress_and_vest.webp'
+import seeFashion8 from '../public/img/physical/seefashion/runway_shot_of_2_models_walking.webp'
+import seeFashion9 from '../public/img/physical/detail_shot_of_a_green_suit.webp'
+import seeFashion10 from '../public/img/physical/seefashion/4_model_full_body_shot.webp'
+import seeFashion11 from '../public/img/physical/seefashion/diverse_model_in_draped_top.webp'
+
+import Breadcrumb from "@/components/Breadcrumb";
+import Carousel from "@/components/Carousel";
+import StaticBrand from "@/components/StaticBrand";
+import PageContainer from "@/components/PageContainer";
+
+import "../public/stylesheets/projects.css";
+
+const images = [
+  { src: seeFashion1, alt: 'video thumbnail' },
+  { src: seeFashion2, alt: 'model bust shot looking into the camera' },
+  { src: seeFashion3, alt: 'diverse model indress' },
+  { src: seeFashion4, alt: 'diverse model looking to the side' },
+  { src: seeFashion5, alt: '3 models looking into the camera' },
+  { src: seeFashion6, alt: 'runway shot of model walking' },
+  { src: seeFashion7, alt: 'diverse model in dress and vest' },
+  { src: seeFashion8, alt: 'runway shot of 2 models walking' },
+  { src: seeFashion9, alt: 'detail shot of a green suit' },
+  { src: seeFashion10, alt: '4 models full body shot' },
+  { src: seeFashion11, alt: 'diverse model in draped top' }
+];
+
+const subtitle = "Hybrid Runway show in 21 - openair showcased at 'Haus der Statistik', Alexanderplatz, Berlin and online as livestream.";
+const descriptionText = "Runway video Fashion & Styling Studio.Stuckn Video Petandflo Location Cinegate Set photography Tiffany Chaves Backstage photography Tabi Charaf Hair & Makeup Mud Studio Berlin Models Viva Models In cooperation with Bluescope Berlin and Kathi Kaeppel Runway soundtrack Cleon";
+const description = { title: "See Fashion", subtitle: subtitle, text: descriptionText };
+
+function Seefashion() {
   return (
-    <h1>seefashion</h1>
-  )
+    <PageContainer>
+      <Breadcrumb />
+      <Carousel images={images} description={description}/>
+      <StaticBrand opacity={"opacity-1"} />
+    </PageContainer>
+  );
 }
+
+export default Seefashion;

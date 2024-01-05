b872a0e7697094b31c16b923243b3f61d92d0f56
Mari Calmer <superlassig@gmail.com>
2024-01-05 18:25:01 +0100

    fix: correct homepage brand bigger sizes towards big font size

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -10,7 +10,7 @@ const handleFontSizing = (scrollValue, baseFontSize) => {
 
 function Brand(props) {
   return (
-    <h1 className="brand-text text-[80px] md:text-[100px] lg:text-[130px] xl:text-[160px] min-[2000px]:text-[195px] font-headline quella border-solid border-b-2 border-[#a1bf79] mt-[70vh] lg:mt-[66vh] xl:mt-[62vh] 2xl:mt-[64vh] min-[2000px]:mt-[66vh] mb-80 mx-14 leading-none w-10/12 sticky top-6 mix-blend-difference">
+    <h1 className="brand-text text-[80px] md:text-[100px] lg:text-[130px] xl:text-[160px] min-[2000px]:text-[235px] font-headline quella border-solid border-b-2 border-[#a1bf79] mt-[70vh] lg:mt-[66vh] xl:mt-[62vh] 2xl:mt-[64vh] min-[2000px]:mt-[62vh] mb-80 mx-14 leading-none w-10/12 sticky top-6 mix-blend-difference">
       <span className="tracking-tight">Stu</span>di<span className="tracking-tight">o.Stuckn</span>
       <style jsx>{`
         h1 {

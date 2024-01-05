d3b9da3ab56e285ff641c86f1550ceae8172f49e
Mari Calmer <superlassig@gmail.com>
2024-01-05 19:11:18 +0100

    feat: add links to navlist

--- components/NavList.jsx
+++ components/NavList.jsx
@@ -1,14 +1,15 @@
 // NavList.jsx
 'use client';
 import React from "react";
+import Link from 'next/link'
 
 function NavList() {
   return (
-    <ul className="navlist__container translate-y-[-150%] flex helvetica place-content-between w-[80%] pt-8 pl-14 leading-none transition-[transform] delay-300">
+    <ul className="navlist__container translate-y-[-150%] flex items-baseline helvetica place-content-between w-[80%] pt-8 pl-14 leading-none transition-[transform] delay-300">
       <li className="navlist__item opacity-0 transition-[opacity] duration-1000 ease">Ronja Stucken</li>
-      <li className="navlist__item cursor-pointer hover:text-[#C9D1D2] opacity-0 transition-[opacity] duration-200 ease">Instagram</li>
-      <li className="navlist__item cursor-pointer hover:text-[#C9D1D2] opacity-0 transition-[opacity] duration-200 ease">Linkedin</li>
-      <li className="navlist__item cursor-pointer hover:text-[#C9D1D2] opacity-0 transition-[opacity] duration-200 ease">studio@ronjastucken.com</li>
+      <li className="my-2"><a href="https://www.instagram.com/studio.stuckn" target="_blank" className="navlist__item cursor-pointer hover:text-[#C9D1D2] opacity-0 transition-[opacity] duration-200 ease">Instagram</a></li>
+      <li><a href="https://de.linkedin.com/in/ronja-stucken" target="_blank" className="navlist__item cursor-pointer hover:text-[#C9D1D2] opacity-0 transition-[opacity] duration-200 ease">LinkedIn</a></li>
+      <li><Link href="mailto:info@mailgo.dev" className="navlist__item cursor-pointer hover:text-[#C9D1D2] opacity-0 transition-[opacity] duration-200 ease">studio@ronjastucken.com</Link></li>
     </ul>
   );
 }

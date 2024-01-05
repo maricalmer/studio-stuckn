ee79adb3d9dfa472ebe4c0f0c69ffa85703b82ec
Mari Calmer <superlassig@gmail.com>
2024-01-05 17:57:34 +0100

    feat: adapt top margin spacing to screen size for homepage brand

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -10,7 +10,7 @@ const handleFontSizing = (scrollValue, baseFontSize) => {
 
 function Brand(props) {
   return (
-    <h1 className="brand-text text-[80px] md:text-[100px] lg:text-[130px] xl:text-[160px] font-headline quella border-solid border-b-2 border-[#a1bf79] mb-80 mx-14 leading-none w-10/12 sticky top-6 mix-blend-difference">
+    <h1 className="brand-text text-[80px] md:text-[100px] lg:text-[130px] xl:text-[160px] font-headline quella border-solid border-b-2 border-[#a1bf79] mt-[70vh] md:mt-[68vh] lg:mt-[64vh] xl:mt-[60vh] 2xl:mt-[55vh] min-[1800px]:mt-[55vh] mb-80 mx-14 leading-none w-10/12 sticky top-6 mix-blend-difference">
       <span className="tracking-tight">Stu</span>di<span className="tracking-tight">o.Stuckn</span>
       <style jsx>{`
         h1 {

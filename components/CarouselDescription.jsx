e5cbee2719e503465a2cab08e738150a6867cfb4
Mari Calmer <superlassig@gmail.com>
2024-01-12 17:46:08 +0100

    fix: add bottom margin to project title on mobile

--- components/CarouselDescription.jsx
+++ components/CarouselDescription.jsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 const CarouselDescription = ({ title, subtitle, details, softwares, credits, previousProject, nextProject }) => {
   return (
     <div className="mb-8 md:w-[220px] h-fit md:h-[60vh] md:fixed left-[100px]">
-      <div className="quella text-4xl mt-5 relative z-20 md:p-3">{title}</div>
-      {subtitle && <div className={subtitle.cyrillic ? "helvetica text-sm mt-3 mb-3 relative z-20 leading-4 md:mt-0 md:px-3 font-bold" : "helvetica text-sm mb-3 mt-3 relative z-20 leading-4 md:mt-0 md:px-3"}>{subtitle.text}</div>}
+      <div className="quella text-4xl mt-5 mb-5 relative z-20 md:p-3 md:mb-0">{title}</div>
+      {subtitle && <div className={subtitle.cyrillic ? "helvetica text-sm mb-3 relative z-20 leading-4 md:px-3 font-bold" : "helvetica text-sm mb-3 relative z-20 leading-4 md:px-3"}>{subtitle.text}</div>}
       <div className="helvetica text-sm mb-3 relative z-20 leading-4 md:px-3">{details}</div>
       {softwares && <div className="helvetica text-sm relative z-20 leading-4 md:px-3">{softwares}</div>}
       {credits && credits.map((credit, index) => {

045950370245b70d4d3a988bc6a06f84ae8d7fef
Mari Calmer <superlassig@gmail.com>
2024-01-18 20:29:26 +0100

    refacto: uniform props in alien-accessories

--- pages/alien-accessories.jsx
+++ pages/alien-accessories.jsx
@@ -1,4 +1,7 @@
-'use client';
+import Breadcrumb from "@/components/Breadcrumb";
+import Carousel from "@/components/Carousel";
+import StaticBrand from "@/components/StaticBrand";
+import PageContainer from "@/components/PageContainer";
 import blackBootsProfileImg from '../public/img/digital/alien-accessories/3d_black_boots_profile.webp'
 import greenBagProfileImg from '../public/img/digital/alien-accessories/3d_green_bag_profile.webp'
 import whiteBagDetailImg from '../public/img/digital/alien-accessories/3d_white_bag_detail.webp'
@@ -14,10 +17,6 @@ import silverBootsFrontImg from '../public/img/digital/alien-accessories/3d_silv
 import greenBagSideImg from '../public/img/digital/alien-accessories/3d_green_bag_side.webp'
 import orangeSneakerTopImg from '../public/img/digital/alien-accessories/3d_orange_sneaker_top.webp'
 import orangeSneakerDetailImg from '../public/img/digital/alien-accessories/3d_orange_sneaker_detail.webp'
-import Breadcrumb from "@/components/Breadcrumb";
-import Carousel from "@/components/Carousel";
-import StaticBrand from "@/components/StaticBrand";
-import PageContainer from "@/components/PageContainer";
 import "../public/stylesheets/projects.css";
 
 const elements = [
@@ -49,7 +48,7 @@ function AlienAccessories() {
     <PageContainer>
       <Breadcrumb textColor="text-black" />
       <Carousel description={description} elements={elements} />
-      <StaticBrand extraStyling={"opacity-1 border-black"} />
+      <StaticBrand extraStyling="opacity-1 border-black" />
     </PageContainer>
   );
 }

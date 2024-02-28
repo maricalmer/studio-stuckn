f9e6ef015180ad7134ddf85e14a7b1a0745b87a1
Mari Calmer <superlassig@gmail.com>
2024-02-28 22:08:03 +0100

    fix revert fonts to otf

--- components/PageContainer.jsx
+++ components/PageContainer.jsx
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 
-const quella = localFont({src: '../public/fonts/Quella.woff', variable: '--quella'});
-const helvetica = localFont({src: '../public/fonts/Helvetica.woff', variable: '--helvetica'});
+const quella = localFont({src: '../public/fonts/Quella.otf', variable: '--quella'});
+const helvetica = localFont({src: '../public/fonts/Helvetica.otf', variable: '--helvetica'});
 
 export default function PageContainer({ children, backgroundColor }) {
   return (

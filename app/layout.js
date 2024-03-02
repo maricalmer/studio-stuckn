126424a03741da3c3b9970370eaee0269a203cd9
Mari Calmer <superlassig@gmail.com>
2024-03-02 13:47:07 +0100

    fix: helvetica in woff2 and quella in otf

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "../public/stylesheets/globals.css";
 import localFont from 'next/font/local'
 
 const quella = localFont({src: '../public/fonts/Quella.otf', variable: '--quella'})
-const helvetica = localFont({src: '../public/fonts/Helvetica.otf', variable: '--helvetica'});
+const helvetica = localFont({src: '../public/fonts/Helvetica.woff2', variable: '--helvetica'});
 
 export default function RootLayout({ children }) {
   return (

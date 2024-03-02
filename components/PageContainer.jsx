1685f8ddf55eec7da941caabb21bf95c7cb68b90
Mari Calmer <superlassig@gmail.com>
2024-03-02 13:47:37 +0100

    fix: helvetica in woff2 and quella in otf

--- components/PageContainer.jsx
+++ components/PageContainer.jsx
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 
 const quella = localFont({src: '../public/fonts/Quella.otf', variable: '--quella'});
-const helvetica = localFont({src: '../public/fonts/Helvetica.otf', variable: '--helvetica'});
+const helvetica = localFont({src: '../public/fonts/Helvetica.woff2', variable: '--helvetica'});
 
 export default function PageContainer({ children, backgroundColor }) {
   return (

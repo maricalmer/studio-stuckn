6f38960cb7cccc25254d8ce787cc43da555587e0
Mari Calmer <superlassig@gmail.com>
2024-03-14 16:18:27 +0100

    feat: shorten meta description

--- app/layout.js
+++ app/layout.js
@@ -9,18 +9,18 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${quella.variable} ${helvetica.variable}`}>
       <head>
         <title>Studio.Stuckn, 3D artist based in Berlin</title>
-        <meta name="description" content="Explore 3D artist Ronja Stucken's portfolio, showcasing expertise in digital fashion design, avatars, scene building, as well as proficiency in physical fashion design and pattern making."></meta>
+        <meta name="description" content="Discover 3D artist Ronja Stucken's portfolio, featuring expertise in digital fashion design, avatars, scene building, and physical fashion design."></meta>
         <meta itemProp="name" content="Studio.Stuckn"></meta>
-        <meta itemProp="description" content="Explore 3D artist Ronja Stucken's portfolio, showcasing expertise in digital fashion design, avatars, scene building, as well as proficiency in physical fashion design and pattern making."></meta>
+        <meta itemProp="description" content="Discover 3D artist Ronja Stucken's portfolio, featuring expertise in digital fashion design, avatars, scene building, and physical fashion design."></meta>
         <meta itemProp="image" content="https://res.cloudinary.com/dq41jyzzc/image/upload/v1705505443/meta_img.png"></meta>
         <meta property="og:url" content="https://www.studiostuckn.com"></meta>
         <meta property="og:type" content="website"></meta>
         <meta property="og:title" content="Studio.Stuckn"></meta>
-        <meta property="og:description" content="Explore 3D artist Ronja Stucken's portfolio, showcasing expertise in digital fashion design, avatars, scene building, as well as proficiency in physical fashion design and pattern making."></meta>
+        <meta property="og:description" content="Discover 3D artist Ronja Stucken's portfolio, featuring expertise in digital fashion design, avatars, scene building, and physical fashion design."></meta>
         <meta property="og:image" content="https://res.cloudinary.com/dq41jyzzc/image/upload/v1705505443/meta_img.png"></meta>
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="twitter:title" content="Studio.Stuckn"></meta>
-        <meta name="twitter:description" content="Explore 3D artist Ronja Stucken's portfolio, showcasing expertise in digital fashion design, avatars, scene building, as well as proficiency in physical fashion design and pattern making."></meta>
+        <meta name="twitter:description" content="Discover 3D artist Ronja Stucken's portfolio, featuring expertise in digital fashion design, avatars, scene building, and physical fashion design."></meta>
         <meta name="twitter:image" content="https://res.cloudinary.com/dq41jyzzc/image/upload/v1705505443/meta_img.png"></meta>
         <meta name="google-site-verification" content="KSG4ALn4wQN7SBpS0FrUDlyIa7-nYg3vfHvPGznHx2k"></meta>
       </head>
